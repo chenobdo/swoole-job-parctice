5e985cca9c269eb91277da337a5e9d49ddef76e4
Gabriel <obdo.briel@gmail.com>
2017-06-15 10:25:53 +0800

    fix

--- src/Process.php
+++ src/Process.php
@@ -55,7 +55,7 @@ class Process
 			$this->exitMaster("收到退出信号,退出主进程");
 		});
 
-		\swoole_process::signal(SIGCHLD, function ($signo) use (&$workers)) {
+		\swoole_process::signal(SIGCHLD, function ($signo) use (&$workers) {
 			while (true) {
 				$ret = \swoole_process::wait(false);
 				if ($ret) {
@@ -68,7 +68,7 @@ class Process
                     unset($workers[$pid]);
 				}
 			}
-		}
+		});
 	}
 
 	private function exitMaster()
